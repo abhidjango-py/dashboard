c64d8902b0193833b28daab80b3e7acdf2d89aa0
abhidjango-py <abhishekmishra45658@gmail.com>
2024-02-27 11:04:00 +0530

    routes settled

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import Loader from "./components/Loader";
+const Dashboard = lazy(() => import("./pages/dashboard"));
+const Products = lazy(() => import("./pages/products"));
+const Customers = lazy(() => import("./pages/customers"));
+const Transactions = lazy(() => import("./pages/transactions"));
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/products" element={<Products />} />
+          <Route path="/admin/transactions" element={<Transactions />} />
+          <Route path="/admin/customers" element={<Customers />} />
+
+          {/* charts */}
+
+          {/* apps */}
+        </Routes>
+      </Suspense>
+    </Router>
+  );
 };
 
 export default App;
