61a384e439193208036d15a744c998cac6370fdc
abhishek10-05 <abhishekmishra45658@gmail.com>
2024-03-03 00:03:56 +0530

    search bar completed

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,21 @@
+import { BsSearch } from "react-icons/bs";
 import AdminSidebar from "../components/AdminSidebar";
-
+import { FaRegBell } from "react-icons/fa";
+import userImg from "../assets/userimg.png";
 const dashboard = () => {
   return (
     <div className="adminContainer">
       {/* Sidebar */}
       <AdminSidebar />
       {/* main */}
-      <main>mainmainmainmain</main>
+      <main className="dashboard">
+        <div className="bar">
+          <BsSearch/>
+          <input type="text" placeholder="Search for data, users, docs" />
+          <FaRegBell/>
+          <img src={userImg} alt="acc_icon" />
+        </div>
+      </main>
     </div>
   );
 };
