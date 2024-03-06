017fda85401d7a4508fe3977bfff1d50d069e77f
abhishek10-05 <abhishekmishra45658@gmail.com>
2024-03-06 07:13:01 +0530

    transactionManagement completed

--- src/pages/management/transactionManagement.tsx
+++ src/pages/management/transactionManagement.tsx
@@ -1,12 +1,118 @@
+import { useState } from "react";
 import AdminSidebar from "../../components/AdminSidebar";
+import { OrderItemType, OrderType } from "../../types";
+import { Link } from "react-router-dom";
+const img =
+  "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=804";
 
+const orderItems: OrderItemType[] = [
+  {
+    name: "Puma Shoes",
+    photo: img,
+    _id: "SHA9790",
+    quantity: 4,
+    price: 2000,
+  },
+];
 const transactionManagement = () => {
+  const [order, setOrder] = useState<OrderType>({
+    name: "Abhishek Mishra",
+    address: "77 Black Street",
+    city: "Neyword",
+    state: "Neward",
+    country: "India",
+    pincode: 208716,
+    status: "Processing",
+    subtotal: 4000,
+    discount: 1200,
+    shippingCharges: 0,
+    tax: 200,
+    total: 4000 + 200 + 0 - 1200,
+    orderItems,
+    _id: "SHA18716",
+  });
+
+  const {
+    address,
+    city,
+    country,
+    discount,
+    name,
+    pincode,
+    shippingCharges,
+    state,
+    status,
+    subtotal,
+    tax,
+    total,
+  } = order;
+
+  const updateHandler = () => {
+    setOrder((prev) => ({
+      ...prev,
+      status: prev.status === "Processing" ? "Shipped" : "Delivered",
+    }));
+  };
+
   return (
     <div className="adminContainer">
       <AdminSidebar />
-      <main className="productManagement">TransactionManagement</main>
+      <main className="productManagement">
+        <section style={{ padding: "2rem" }}>
+          <h2>Order Items</h2>
+          {order.orderItems.map((i) => (
+            <ProductCard
+              name={i.name}
+              photo={i.photo}
+              _id={i._id}
+              quantity={i.quantity}
+              price={i.price}
+            />
+          ))}
+        </section>
+        <article className="shippingInfoCard">
+          <h1>Order Info</h1>
+          <h5>User Info</h5>
+          <p>Name: {name}</p>
+          <p>
+            Address : {`${address}, ${city}, ${state}, ${country} ${pincode}`}
+          </p>
+          <h5>Amount Info</h5>
+          <p>Subtotal: {subtotal}</p>
+          <p>Shipping Charges: {shippingCharges}</p>
+          <p>Tax: {tax}</p>
+          <p>Discount: {discount}</p>
+          <p>Total: {total}</p>
+          <h5>Status Info</h5>
+          <p>
+            Status:{" "}
+            <span
+              className={
+                status === "Delivered"
+                  ? "purple"
+                  : status === "Shipped"
+                  ? "green"
+                  : "red"
+              }
+            >
+              {status}
+            </span>
+          </p>
+          <button onClick={updateHandler}>Process Status</button>
+        </article>
+      </main>
     </div>
   );
 };
+
+const ProductCard = ({ name, _id, photo, price, quantity }: OrderItemType) => (
+  <div className="transactionProductCard">
+    <img src={photo} alt={name} />
+    <Link to={`/product/${_id}`}>{name}</Link>
+    <span>
+      ${price} X {quantity}= ${quantity * price}
+    </span>
+  </div>
+);
 
 export default transactionManagement;
