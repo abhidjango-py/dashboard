480dbfc019ffd899821356189c0467de2d4640aa
abhishek10-05 <abhishekmishra45658@gmail.com>
2024-03-14 20:48:00 +0530

    stopwatch completed

--- src/pages/stopwatch.tsx
+++ src/pages/stopwatch.tsx
@@ -1,12 +1,55 @@
+import { useEffect, useState } from "react";
 import AdminSidebar from "../components/AdminSidebar";
 
+const formatTime = (timeInSeconds: number) => {
+  const hours = String(Math.floor(timeInSeconds / 3600));
+  const minutes = String(Math.floor((timeInSeconds % 3600) / 60));
+  const seconds = String(timeInSeconds % 60);
+
+  return `${hours.padStart(2, "0")}:${minutes.padStart(
+    2,
+    "0"
+  )}:${seconds.padStart(2, "0")}`;
+};
+
 const stopwatch = () => {
+  const [time, setTime] = useState<number>(0);
+  const [isRunning, setIsRunning] = useState<boolean>(false);
+
+  const resetHandler = () => {
+    setTime(0);
+    setIsRunning(false);
+  };
+
+  useEffect(() => {
+    let intervalID: number;
+    if (isRunning) {
+      intervalID = setInterval(() => {
+        setTime((prev) => prev + 1);
+      }, 1000);
+    }
+    return () => {
+      clearInterval(intervalID);
+    };
+  }, [isRunning]);
+
   return (
     <div className="adminContainer">
       {/* Sidebar */}
       <AdminSidebar />
       {/* main */}
-      <main>mainmainmainmain</main>
+      <main className="dashboardAppContainer">
+        <h1>Stopwatch</h1>
+        <section>
+          <div className="stopwatch">
+            <h2>{formatTime(time)}</h2>
+            <button onClick={() => setIsRunning((prev) => !prev)}>
+              {isRunning ? "Stop" : "Start"}
+            </button>
+            <button onClick={resetHandler}>Reset</button>
+          </div>
+        </section>
+      </main>
     </div>
   );
 };
